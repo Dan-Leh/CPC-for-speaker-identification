9853e983d3036663378f07c666c5f0131f61fe75
TLeloup <t.r.leloup@student.tue.nl>
2023-05-28 11:18:41 +0200

    Functional Patching pipeline

--- utils/data_CPC.py
+++ utils/data_CPC.py
@@ -4,11 +4,13 @@ import time
 import numpy as np
 import torchaudio
 from torch.utils.data import Dataset
+from utils.config import Config
+cfg = Config()
 
 class LibriDataset(Dataset):
-    def __init__(self, split='train', patch_size = 0.1, n_predictions = 20):
+    def __init__(self, split='train', patch_size = cfg.patch_size, n_predictions = cfg.n_predictions):
         assert split in ['train', 'test'], 'Only train and test splits are implemented.'
-        self.patch_size = np.around(patch_size*16000)
+        self.patch_size = int(np.rint(patch_size*16000))
         self.n_predictions = n_predictions
         data_dir = './data' # root directory for data
 
@@ -24,8 +26,6 @@ class LibriDataset(Dataset):
         for i, filename in enumerate(self.filename_list):
             split_name = filename.split('-')
             self.filepath_list[i] += os.path.join(split_name[0], split_name[1], filename+'.flac')
-        
-        self.speakerID_list = sorted(np.array(os.listdir(Libri_dir), dtype=np.uint32)) #list of speaker IDs in ascending order
 
     # Returns the length of the dataset
     def __len__(self):
@@ -50,14 +50,13 @@ class LibriDataset(Dataset):
             IDs.append(self.filename_list[idx].split('-')[0])
         return IDs
     
-    def delete_small_items(self):
+    def __delete_small_items__(self):
         lengths = self.get_all_items()
         for idx in range(self.__len__()):
             if lengths[idx] < self.patch_size*(self.n_predictions+1):
                 del self.filepath_list[idx]
                 del self.filename_list[idx]
-                del self.speakerID_list[idx]
-
+        return self
 
     def crop_audio(self, waveform):
         waveform = waveform.squeeze() # get rid of channel dimension
@@ -88,7 +87,8 @@ class LibriDataset(Dataset):
         waveform = self.crop_audio(waveform) #take random crop of sample
         patches = self.split_patches(waveform) # split into patches
         mfcc_transform = torchaudio.transforms.MFCC(sample_rate, n_mfcc=64, melkwargs={"n_fft": 474}) # n_mfcc and n_fft chosen so that images are of resolution 64x96
-        mfcc_spectrograms = map(lambda patch: normalize(mfcc_transform(patch)), patches) # apply mfcc transform to and normalize each patch
+        mfcc_spectrograms = map(lambda patch: mfcc_transform(patch), patches) # apply mfcc transform to each patch
+        mfcc_spectrograms = map(lambda patch: self.normalize(patch), mfcc_spectrograms) #  normalize each patch
         mfcc_spectrograms = list(mfcc_spectrograms) # convert map object to list
 
 
